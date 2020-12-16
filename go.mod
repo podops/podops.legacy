cb2775abdd206a719d5e8a481786aa3f35a57ed8
Michael Kuehl <hello@txs.vc>
2020-12-16 23:16:15 +0100

    basic yaml to XML conversion

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
 module github.com/podops/podops
 
-go 1.14
+go 1.15
 
 require (
+	github.com/eduncan911/podcast v1.4.2
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-contrib/sessions v0.0.3
 	github.com/gin-gonic/gin v1.6.3
@@ -11,4 +12,5 @@ require (
 	github.com/txsvc/service v0.11.1
 	github.com/urfave/cli v1.22.5
 	google.golang.org/appengine v1.6.7
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776
 )
---
+++ pkg/metadata/metadata.go
@@ -0,0 +1,93 @@
+package metadata
+
+import (
+	"time"
+
+	"github.com/eduncan911/podcast"
+)
+
+type (
+	Metadata struct {
+		Name   string
+		Labels map[string]string `yaml:"labels,omitempty"`
+	}
+
+	Link struct {
+		URI   string
+		Title string `yaml:"title,omitempty"`
+		Rel   string `yaml:"rel,omitempty"`
+		Type  string `yaml:"type,omitempty"`
+	}
+
+	Image struct {
+		URI    string
+		Title  string `yaml:"title,omitempty"`
+		Anchor string `yaml:"anchor,omitempty"`
+	}
+
+	Contributor struct {
+		Name string
+		URI  string `yaml:"uri,omitempty"`
+	}
+
+	Owner struct {
+		Name  string
+		Email string
+	}
+
+	Description struct {
+		Title     string
+		Link      string
+		SubTitle  string `yaml:"subtitle,omitempty"`
+		Summary   string `yaml:"summary,omitempty"`
+		Copyright string `yaml:"copyright,omitempty"`
+		Author    string
+		Owner     Owner
+	}
+
+	Content struct {
+		Category string
+		Type     string
+		Language string `yaml:"language,omitempty"`
+		Explicit string `yaml:"explicit,omitempty"`
+		Block    string `yaml:"block,omitempty"`
+	}
+
+	// Show holds all metadata related to a podcast/show
+	Show struct {
+		APIVersion   string `yaml:"apiVersion"`
+		Kind         string
+		Metadata     Metadata `yaml:"metadata,omitempty"`
+		Description  Description
+		Content      Content
+		Image        Image
+		Links        []Link        `yaml:"links,omitempty"`
+		Contributors []Contributor `yaml:"contributors,omitempty"`
+	}
+)
+
+func (l *Link) ToAtomLink() *podcast.AtomLink {
+	return &podcast.AtomLink{
+		HREF: l.URI,
+		Rel:  l.Rel,
+		Type: l.Type,
+	}
+}
+func (show *Show) ToPodcast() (*podcast.Podcast, error) {
+	now := time.Now()
+	// basics
+	p := podcast.New(show.Description.Title, show.Description.Link, show.Description.Summary, &now, &now)
+	// details
+	p.AddSubTitle(show.Description.SubTitle)
+	p.AddSummary(show.Description.Summary)
+	p.AddAuthor(show.Description.Owner.Name, show.Description.Owner.Email)
+	p.AddCategory(show.Content.Category, nil)
+	p.AddImage(show.Image.URI)
+	/*
+		// add atom links
+		for _, link := range show.Links {
+			p.AddAtomLink(link.URI)
+		}
+	*/
+	return &p, nil
+}
