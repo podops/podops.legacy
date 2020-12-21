c6b08e9031f38a82f6963389260371ac2640fbb3
Michael Kuehl <hello@txs.vc>
2020-12-21 15:37:16 +0100

    asimilated the podcast feed stuff into the codebase

--- internal/rss/types.go
+++ internal/rss/types.go
@@ -14,12 +14,9 @@ type AtomLink struct {
 
 // Image represents an image.
 //
-// Podcast feeds contain artwork that is a minimum size of
-// 1400 x 1400 pixels and a maximum size of 3000 x 3000 pixels,
-// 72 dpi, in JPEG or PNG format with appropriate file
-// extensions (.jpg, .png), and in the RGB colorspace. To optimize
-// images for mobile devices, Apple recommends compressing your
-// image files.
+// Podcast feeds contain artwork that is a minimum size of 1400 x 1400 pixels and a maximum size of 3000 x 3000 pixels,
+// 72 dpi, in JPEG or PNG format with appropriate file extensions (.jpg, .png), and in the RGB colorspace. To optimize
+// images for mobile devices, Apple recommends compressing your image files.
 type Image struct {
 	XMLName     xml.Name `xml:"image"`
 	URL         string   `xml:"url"`
