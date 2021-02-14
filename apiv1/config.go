65f8d720296455b08dda4bfcb94e09c0ca49f2cb
Michael Kuehl <hello@txs.vc>
2021-02-14 10:39:44 +0100

    reverted the way cdn links are created, it is up to the client to provide the correct cdn prefix

--- apiv1/config.go
+++ apiv1/config.go
@@ -5,13 +5,15 @@ import (
 )
 
 const (
-	defaultBucketUpload     = "upload.podops.dev"
+
+	//defaultBucketUpload     = "upload.podops.dev"
 	defaultBucketProduction = "production.podops.dev"
 	defaultBucketCDN        = "cdn.podops.dev"
 
-	defaultPortalEndpoint = "https://podops.dev"
-	defaultAPIEndpoint    = "https://api.podops.dev"
-	defaultCDNEndpoint    = "https://cdn.podops.dev"
+	defaultPortalEndpoint  = "https://podops.dev"
+	defaultAPIEndpoint     = "https://api.podops.dev"
+	defaultCDNEndpoint     = "https://cdn.podops.dev"
+	defaultStorageEndpoint = "https://storage.googleapis.com/cdn.podops.dev"
 )
 
 var (
@@ -25,11 +27,14 @@ var (
 	DefaultCDNEndpoint string = env.GetString("CDN_URL", defaultCDNEndpoint)
 
 	// BucketUpload is the canonical name of the upload bucket
-	BucketUpload string = env.GetString("BUCKET_UPLOAD", defaultBucketUpload)
+	//BucketUpload string = env.GetString("BUCKET_UPLOAD", defaultBucketUpload)
 
 	// BucketProduction is the canonical name of the production bucket
 	BucketProduction string = env.GetString("BUCKET_PRODUCTION", defaultBucketProduction)
 
 	// BucketCDN is the canonical name of the CDN bucket
 	BucketCDN string = env.GetString("BUCKET_CDN", defaultBucketCDN)
+
+	// StorageEndpoint is the direct link to assets in Google Storage
+	StorageEndpoint string = env.GetString("STORAGE_ENDPOINT", defaultStorageEndpoint)
 )
