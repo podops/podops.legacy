9c1cc0c3c480b1a9453974d2c463ea21201d609c
Michael Kuehl <hello@txs.vc>
2021-03-06 09:59:52 +0100

    updated version strings

---
+++ apiv1/version.go
@@ -0,0 +1,22 @@
+package apiv1
+
+import "fmt"
+
+const (
+	// Version specifies the verion of the API and its structs
+	Version = "v1"
+
+	// MajorVersion of the API
+	MajorVersion = 0
+	// MinorVersion of the API
+	MinorVersion = 9
+	// FixVersion of the API
+	FixVersion = 2
+)
+
+var (
+	// VersionString is the canonical API description
+	VersionString string = fmt.Sprintf("%d.%d.%d", MajorVersion, MinorVersion, FixVersion)
+	// UserAgentString identifies any http request podops makes
+	UserAgentString string = fmt.Sprintf("PodOps %d.%d.%d", MajorVersion, MinorVersion, FixVersion)
+)
