d787ffc6841d2d7cca32366eb94c15aa5cf507c5
Michael Kuehl <hello@txs.vc>
2021-03-06 17:47:33 +0100

    switched to the platform provider impl for error reporting

--- cmd/cdn/main.go
+++ cmd/cdn/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/fupas/commons/pkg/env"
+	"github.com/fupas/platform"
 	svc "github.com/fupas/platform/pkg/http"
+	gcp "github.com/fupas/platform/provider/google"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/podops/podops/internal/api"
@@ -66,6 +70,19 @@ func shutdown(*echo.Echo) {
 }
 
 func init() {
+	// initialize the platform first
+	projectID := env.GetString("PROJECT_ID", "")
+	if projectID == "" {
+		log.Fatal("Missing variable 'PROJECT_ID'")
+	}
+	serviceName := env.GetString("SERVICE_NAME", "default")
+
+	client, err := platform.NewClient(context.Background(), gcp.NewErrorReporting(context.TODO(), projectID, serviceName))
+	if err != nil {
+		log.Fatal("error initializing the platform services")
+	}
+	platform.RegisterGlobally(client)
+
 	staticFileLocation = env.GetString("STATIC_FILE_LOCATION", "public")
 }
 
